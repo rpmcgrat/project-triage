c2c716994bff52ccacbacf79f08d868416c1a734
Riley McGrath <rpmcgrat@iu.edu>
2019-09-23 08:45:41 -0400

    update

---
+++ app/public/js/record.app.js
@@ -0,0 +1,38 @@
+var patientRecordApp = new Vue({
+  el: '#patientRecordApp',
+  data: {
+    patients:
+        {
+        firstName: "",
+        lastName: "",
+        dob: "",
+        sexAtBirth: ""
+        }
+  },
+  methods: {
+    fetchPatients() {
+      fetch('dummy.php')
+      .then(response => response.json())
+      .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+    // TODO: post the form contents somewhere
+    // fetch(url, {method:"POST", data:this.formPatient})
+    // .then(...)
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+        firstName: "",
+        lastName: "",
+        dob: "",
+        sexAtBirth: ""
+      }
+    },
+    selectRecord(patient) {
+      
+    }
+
+  },
+  created() {
+    this.fetchPatients();
+  }
+});
